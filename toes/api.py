373f87f65be9dc66fd617bc300d8386f4971eea7
aditya <adityamali33@gmail.com>
2021-01-05 05:45:45 +0530

    new migrations

--- toes/api.py
+++ toes/api.py
@@ -1,34 +1,24 @@
-
-# # Download the helper library from https://www.twilio.com/docs/python/install
-# import os
-# from twilio.rest import Client
-
-
-# # Your Account Sid and Auth Token from twilio.com/console
-# # and set the environment variables. See http://twil.io/secure
-# account_sid = 'AC9f3176c894b3ba79073dba0146d8b6e3'
-# auth_token = '313cef0fa632d53a2f10afeccd5ee366'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages \
-#                 .create(
-#                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-#                      from_='+19388883481',
-#                      to='+919765402942'
-#                  )
-
-# print(message.sid)
-
 import requests
+import json 
 
-url = "https://www.fast2sms.com/dev/bulk"
-
-querystring = {"authorization":"8SxMu8XjX6rpRasOGDY83AoGQzedmJA7wbgGOEgp92XYsWanQBiUx96IIVeU","sender_id":"FSTSMS","language":"english","route":"qt","numbers":"9765402942","message":"41964","variables":"{#BB#}","variables_values":"654223"}
-
-headers = {
-    'cache-control': "no-cache"
-    }
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+data = {
+            "counter": 0,
+                "isVerified": 0,
+                "is_superuser": 0,
+                "is_admin": 1,
+                "first_name": "admin",
+                "last_name": "lname",
+                "username": "username",
+                "password": "aditya@123",
+                "dob":"1998-02-01",
+                "gender": "male",
+                "aadhar_no": "aadhar",
+                "address":"address",
+                "smartphone": 0,
+                "phone": 987654321456,
+                "re_password": "aditya@123"
+        }
+        
+create_user_api = 'http://52.201.220.252/users/'
+response = requests.post(create_user_api, json=data)
+print("response",response)
