a7cad7fd3484d29191c2bed91010eab2c4c375be
aditya <adityamali33@gmail.com>
2021-01-06 16:26:25 +0530

    new

--- toes/custom_apis/forgot_password_views.py
+++ toes/custom_apis/forgot_password_views.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 from rest_framework.views import APIView
 from .models import VerifyOtp
 from django.core.exceptions import ObjectDoesNotExist
-from .otp_views import send_otp
+
 def enter_otp(request):
     if request.method == 'POST':
         otp = request.POST.get('otp')
@@ -47,12 +47,43 @@ def passreset(request):
     return render(request , 'custom_apis/resetpassword.html')
 
 
+def generateOTP(): 
+    digits = "123456789"
+    OTP = "" 
+    for i in range(6) : 
+        OTP += digits[math.floor(random.random() * 10)] 
+    return OTP 
+
+def send_otp(phone):
+    try:
+        Mobile = User.objects.get(phone=phone) 
+        otp = generateOTP()
+        a = VerifyOtp(phone = phone , otp = otp )
+        a.save()
+        querystring = {"authorization":"8SxMu8XjX6rpRasOGDY83AoGQzedmJA7wbgGOEgp92XYsWanQBiUx96IIVeU","sender_id":"FSTSMS","language":"english","route":"qt","numbers":f"{Mobile}","message":"42422","variables":"{BB}|{FF}","variables_values":f"{otp}|http://52.201.220.252/api/otp"}
+        headers = {
+                    'cache-control': "no-cache"
+                }
+
+        url = "https://www.fast2sms.com/dev/bulk"
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        return Response({"otp": otp}, status=200)  # Just for demonstration
+
+    except ObjectDoesNotExist:
+        message = {
+                'message': 'Phone Number does exist please enter registered phone number'
+            }
+        return Response(data = message, status=400)
+
+
+
 def verify_phone(request):
     if request.method == 'POST':
         phone = request.POST.get('phone')
         try:
             User.objects.get(phone = phone)
-            send_otp(phone=phone)
+            send_otp(phone)
             return redirect('enterotp')
         except ObjectDoesNotExist:
             messages.error(request,'Phone No. Does Not Exist')
