b0c6f24d898995072486dce19df53e6e81c3873d
aditya <adityamali33@gmail.com>
2021-01-06 14:17:54 +0530

    removed zero

--- toes/custom_apis/otp_views.py
+++ toes/custom_apis/otp_views.py
@@ -24,7 +24,7 @@ import json
 
 
 def generateOTP(): 
-    digits = "0123456789"
+    digits = "123456789"
     OTP = "" 
     for i in range(6) : 
         OTP += digits[math.floor(random.random() * 10)] 
