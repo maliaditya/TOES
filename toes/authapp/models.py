fb391edf07734638de52f68ca6f6f3a7db858e62
aditya <adityamali33@gmail.com>
2021-01-10 14:45:19 +0530

    a

--- toes/authapp/models.py
+++ toes/authapp/models.py
@@ -109,7 +109,7 @@ class WorkersRequests(models.Model):
         if time < datetime.datetime.now():
             Event.objects.filter(status=3).update()
     
-    WorkersRequests.Reject_after_ten_minutes()
+WorkersRequests.Reject_after_ten_minutes()  
 
        
 
